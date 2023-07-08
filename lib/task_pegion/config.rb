d22643d3a1747ce7c453ff50ed0cf35784030a34
yuta.onishi <rad6242@icloud.com>
2023-07-09 03:20:40 +0900

    スタイル修正

--- lib/task_pegion/config.rb
+++ lib/task_pegion/config.rb
@@ -1,17 +1,20 @@
 # frozen_string_literal: true
 
-class TaskPegion::Config
-  require 'yaml'
+class TaskPegion
+  # Define the configuration class
+  class Config
+    require 'yaml'
 
-  attr_reader :task_types
+    attr_reader :task_types
 
-  def initialize
-    @task_types = config_file['task_types']
-  end
+    def initialize
+      @task_types = config_file['task_types']
+    end
 
-  private
+    private
 
-  def config_file
-    YAML.load_file('config.yml')
+    def config_file
+      YAML.load_file('config.yml')
+    end
   end
 end
