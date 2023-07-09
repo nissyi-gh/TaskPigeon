e7abc1e619f636caf0b95f74b3c6dfd59cc8cef2
yuta.onishi <rad6242@icloud.com>
2023-07-09 22:22:01 +0900

    task_nameを追加
    何に取り組んだのかわからないので保存します。

--- lib/task_pegion/record.rb
+++ lib/task_pegion/record.rb
@@ -3,11 +3,12 @@
 module TaskPegion
   class Record
     attr_reader :id
-    attr_accessor :task_type, :started_at, :ended_at
+    attr_accessor :task_type, :task_name, :started_at, :ended_at
 
-    def initialize(id: 1, task_type: 'Other', started_at: Time.now, ended_at: nil)
+    def initialize(id: 1, task_type: 'Other', task_name: nil, started_at: Time.now, ended_at: nil)
       @id = id
       @task_type = task_type
+      @task_name = task_name
       @started_at = started_at
       @ended_at = ended_at
     end
