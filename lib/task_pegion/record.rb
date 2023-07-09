d10f5d40bac3657a31bf3e60ac710ccb433825a8
yuta.onishi <rad6242@icloud.com>
2023-07-09 22:32:20 +0900

    CSVを読み込めるようにして、idをCSV基準で作成できるようにする

--- lib/task_pegion/record.rb
+++ lib/task_pegion/record.rb
@@ -2,11 +2,13 @@
 
 module TaskPegion
   class Record
+    require 'csv'
+
     attr_reader :id
     attr_accessor :task_type, :task_name, :started_at, :ended_at
 
-    def initialize(id: 1, task_type: 'Other', task_name: nil, started_at: Time.now, ended_at: nil)
-      @id = new_id
+    def initialize(id: new_id, task_type: 'Other', task_name: nil, started_at: Time.now, ended_at: nil)
+      @id = id
       @task_type = task_type
       @task_name = task_name
       @started_at = started_at
