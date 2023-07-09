2da598d1460e5b28d973d3b0dc46f84bb5971913
yuta.onishi <rad6242@icloud.com>
2023-07-09 22:25:42 +0900

    新しいidを取得する
    idが重複しないように対策します。

--- lib/task_pegion/record.rb
+++ lib/task_pegion/record.rb
@@ -6,11 +6,17 @@ module TaskPegion
     attr_accessor :task_type, :task_name, :started_at, :ended_at
 
     def initialize(id: 1, task_type: 'Other', task_name: nil, started_at: Time.now, ended_at: nil)
-      @id = id
+      @id = new_id
       @task_type = task_type
       @task_name = task_name
       @started_at = started_at
       @ended_at = ended_at
+    end
+
+    private
+
+    def new_id
+      CSV.read('records.csv').size
     end
   end
 end
