b4aa793bd01f5804999874fcb9a6e49c5c36278a
yuta.onishi <rad6242@icloud.com>
2023-07-10 01:43:40 +0900

    経過時間をメソッド化し、通知から呼びやすくする
    Recordクラスで管理し、オブジェクトとしての見通しをよくします。

--- lib/task_pegion.rb
+++ lib/task_pegion.rb
@@ -62,7 +62,7 @@ module TaskPegion
       if record.ended_at
         raise Error, 'Task is already ended'
       else
-        record.ended_at = Time.now
+        record.ended_at = Time.now.to_s
 
         csv_data = CSV.read('records.csv')
         csv_data[-1][4] = record.ended_at
@@ -76,7 +76,11 @@ module TaskPegion
         config = Config.new
         config.destinations.each do |destination|
           if destination['notice_types'].include?('end')
-            Notifier.new(destination['url'], { text: "#{config.user_name}が#{record.task_type}の#{record.task_name}を終了しました。" }).notice
+            text = <<~TEXT
+              #{config.user_name}が#{record.task_type}の#{record.task_name}を終了しました。
+              経過時間は#{record.elapsed_time_formatted}です。
+            TEXT
+            Notifier.new(destination['url'], { text: text }).notice
           end
         end
       end
--- lib/task_pegion/record.rb
+++ lib/task_pegion/record.rb
@@ -26,10 +26,28 @@ module TaskPegion
       new(id: row[0], task_type: row[1], task_name: row[2], started_at: row[3], ended_at: row[4])
     end
 
+    def elapsed_time_formatted
+      "#{elapsed_time[:hours]}時間#{elapsed_time[:minutes]}分#{elapsed_time[:seconds]}秒"
+    end
+
     private
 
     def new_id
       CSV.read('records.csv').size
+    end
+
+    def elapsed_time
+      elapse_seconds = (Time.new(ended_at) - Time.new(started_at)).round
+      hours = elapse_seconds / 3600
+      minutes = (elapse_seconds % 3600) / 60
+      seconds = elapse_seconds % 60
+
+      {
+        total: elapse_seconds,
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds
+      }
     end
   end
 end
