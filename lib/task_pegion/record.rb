1088372eaaeec51621bab6856c685ed2a890ea7b
yuta.onishi <rad6242@icloud.com>
2023-07-09 22:15:05 +0900

    Recodeの初期化にデフォルト値を設定
    必要に応じて上書きする形式にします

--- lib/task_pegion/record.rb
+++ lib/task_pegion/record.rb
@@ -5,8 +5,11 @@ module TaskPegion
     attr_reader :id
     attr_accessor :task_type, :started_at, :ended_at
 
-    def initialize(id)
+    def initialize(id: 1, task_type: 'Other', started_at: Time.now, ended_at: nil)
       @id = id
+      @task_type = task_type
+      @started_at = started_at
+      @ended_at = ended_at
     end
   end
 end
