a7d68c2aa43e37a2e1088deb3600f1131bd5aa89
yuta.onishi <rad6242@icloud.com>
2023-07-09 23:29:44 +0900

    optionを追加
    CLIから呼び出したときにoptionを認識できるようにします

--- lib/task_pegion.rb
+++ lib/task_pegion.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'optionparser'
+
 require_relative "task_pegion/version"
 require_relative 'task_pegion/config'
 require_relative 'task_pegion/record'
@@ -8,4 +10,23 @@ require_relative 'task_pegion/notifier'
 module TaskPegion
   class Error < StandardError; end
   # Your code goes here...
+
+  def self.main
+    options = parse_options
+  end
+
+  def self.parse_options
+    options = {}
+    opt = OptionParser.new do |opt|
+      opt.on_head('-s', '--start', 'Start task') { options[:start] = true }
+      opt.on_head('-e', '--end', 'End task') { options[:end] = true }
+      opt.on('-t TYPE', '--task-type TYPE', 'Task type') { |v| options[:task_type] = v }
+      opt.on('-n NAME', '--task-name NAME', 'Task name') { |v| options[:task_name] = v }
+    end
+
+    opt.parse!(ARGV, into: options)
+    options
+  end
 end
+
+TaskPegion.main
