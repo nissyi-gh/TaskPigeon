dd5c8ba6a71c703b3ce3c7ef23a3ecd9610d4d28
yuta.onishi <rad6242@icloud.com>
2023-07-17 00:43:39 +0900

    タスク開始時にインタラクティブにタスクを実行できるプロンプトクラスを作成
    設定にないタスクタイプを選ばないようにするために実装します

---
+++ lib/task_pegion/cli.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module TaskPegion
+  # Provide functions when run from command line
+  class Cli
+    class StartPrompter
+      attr_reader :config
+
+      def initialize
+        @config = TaskPegion::Config.new
+      end
+
+      def prompter
+        task_type = prompt_task_type
+        task_name = nil
+        puts "task_type: #{task_type}, task_name: #{task_name}"
+      end
+
+      private
+
+      def prompt_task_type
+        show_task_types
+        puts 'Please select task type from above'
+
+        loop do
+          task_type = gets.chomp
+          return task_type if config.task_types.include?(task_type)
+
+          puts 'Please input valid task type'
+        end
+      end
+
+      def show_task_types
+        puts 'Your task types:'
+        config.task_types.each_slice(3) do |task_types|
+          puts task_types.join(' ')
+        end
+        puts ''
+      end
+    end
+  end
+end
