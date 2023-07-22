2b73a320c4a6a8c24032eb171813caf103cc38dd
yuta.onishi <rad6242@icloud.com>
2023-07-22 19:31:07 +0900

    Pomodoroタイマーとして動作するクラスを作成
    集中力を保つ一つとしてタイマーが動くようにします。

---
+++ lib/task_pegion/pomodoro.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module TaskPegion
+  # Pomodoro Timer Class
+  class Pomodoro
+    # 25 minutes
+    WORK_TIME = 1500
+    # 5 minutes
+    BREAK_TIME = 300
+
+    attr_reader :config
+
+    def initialize
+      @config = Config.new
+
+      timer_and_notice
+    end
+
+    private
+
+    def timer_and_notice
+      Signal.trap(:INT) do
+        stop = Cli::StopPrompter.new
+        exit unless stop
+      end
+
+      loop do
+        sleep WORK_TIME
+        @config.destinations.each do |destination|
+          if destination['notice_types'].include?('pomodoro')
+            Notifier.new(destination['url'], { text: 'タスク開始から25分経過しました。一息つきませんか？' }).notice
+          end
+        end
+
+        sleep BREAK_TIME
+        @config.destinations.each do |destination|
+          if destination['notice_types'].include?('pomodoro')
+            Notifier.new(destination['url'], { text: '5分経過しました。タスクに戻りましょう!' }).notice
+          end
+        end
+      end
+    end
+  end
+end
