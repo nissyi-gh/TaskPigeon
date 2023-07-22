297a91af4cd48af50a76f29e9f4c73dd5980f2e4
yuta.onishi <rad6242@icloud.com>
2023-07-23 02:52:01 +0900

    intializeについてテスト
    意図した初期化ができているかを確認します

--- spec/lib/pomodoro_spec.rb
+++ spec/lib/pomodoro_spec.rb
@@ -1,5 +1,24 @@
 # frozen_string_literal: true
 
 RSpec.describe TaskPegion::Pomodoro do
-  # TODO: Write test
+  describe '#initialize' do
+    let(:started_at) { Time.now }
+    let(:pomodoro) do
+      started_at
+      TaskPegion::Pomodoro.new
+    end
+
+    it 'initialize Config' do
+      expect(TaskPegion::Config).to receive(:new)
+      pomodoro
+    end
+
+    it 'sets default values' do
+      expect(pomodoro.started_at).to be_within(1).of(started_at)
+      expect(pomodoro.interrupted).to eq(false)
+      expect(pomodoro.elapsed_time).to eq(0)
+      expect(pomodoro.next_notice_time).to eq(TaskPegion::Pomodoro::WORK_TIME)
+      expect(pomodoro.next_notice_type).to eq('break')
+    end
+  end
 end
